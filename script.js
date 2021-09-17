fd735312607e6ac7373da59e2f6518980d599ad1
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-09-17 19:45:51 -0400

    added time-sensitive coloration to timeblocks.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-let el9 = $('input[data-time="9"]')
+// let el9 = $('input[data-time="9"]')
 
-console.log(el9)
+let currentDayEl = $('#currentDay');
+console.log(currentDayEl);
+
+currentDayEl.text(
+    moment().format("dddd, MMMM Do, YYYY")
+)
+
     // Make a current day el and add a moment to it displaying the day.
 
 // WHEN I scroll down
@@ -15,6 +21,21 @@ console.log(el9)
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
     // style timeblocks here, using conditionals based on current time (re: moment)
+
+    let timeBlockEl = $('[class="form-control"]');
+    console.log(timeBlockEl);
+
+    for (let i = 0; i < timeBlockEl.length; i++){
+       if(timeBlockEl[i].dataset.time*1 < moment().format("H")*1){
+        timeBlockEl[i].style.backgroundColor = "gray";
+       };
+
+       if (timeBlockEl[i].dataset.time*1 === moment().format("H")*1){
+        timeBlockEl[i].style.backgroundColor = "green";
+       }
+       
+   //^Why did I have to *1 to make this work??!!!?
+    }
 
 // WHEN I click into a timeblock
 // THEN I can enter an event
